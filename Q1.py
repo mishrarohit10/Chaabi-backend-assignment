c1fb1c9aed26d9c03a3d5da9eb813a5591272dcf
mishrarohit10 <mishrarohit316x@gmail.com>
2023-06-06 12:03:55 +0530

    q1 added

---
+++ Q1.py
@@ -0,0 +1,22 @@
+def selectionSort(list,size):
+    for i in range(size):
+        min_idx = i
+
+        for j in range(i + 1, size):
+            if list[j] < list[min_idx]:
+                min_idx = j
+
+        (list[i], list[min_idx]) = (list[min_idx],list[i])
+
+list = []
+n = int(input("Enter number of elements"))
+
+for i in range(0,n):
+    element = int(input())
+    list.append(element)
+
+size = len(list)
+selectionSort(list,size)
+
+print(list)
+
