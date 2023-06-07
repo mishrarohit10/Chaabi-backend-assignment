0e5d06b70be2e82cca68056a7faa7216cfe77f3b
mishrarohit10 <mishrarohit316x@gmail.com>
2023-06-08 00:39:21 +0530

    updated input option for Q6

--- Q6.py
+++ Q6.py
@@ -1,7 +1,15 @@
+n = int(input("Enter number of elements : "))
+lists = []
+for i in range(0, n):
+    ele = int(input())
+    lists.append(ele)
 
-lists = [2,3,5,7,11,13,17,19,23,29,31,37,41]
+idx1 = int(input("enter starting"))
+idx2 = int(input("enter end index"))
 
+# lists = [2,3,5,7,11,13,17,19,23,29,31,37,41] 
+# idx1 = 2 , idx = 9
 def f(lists, idx1, idx2):
     print(lists[idx1:idx2:2])
 
-f(lists,2,9)
+f(lists,idx1,idx2)
