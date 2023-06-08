088efc83929dc764528a164a8f4a64619452f990
mishrarohit10 <mishrarohit316x@gmail.com>
2023-06-08 11:05:24 +0530

    updated Q8

--- Q8.py
+++ Q8.py
@@ -1,6 +1,5 @@
 from functools import reduce
 
-
 A0 = dict(zip(('a','b','c','d','e'),(1,2,3,4,5)))
 #output = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
 A1 = range(10)
@@ -19,4 +18,5 @@ A7 = reduce(lambda x,y: x+y, [10,23, -45, 33])
 #output = 21
 A8 = map(lambda x: x*2, [1,2,3,4])
 #output = <map object at 0x000001DA7689B310>
-# A9 = filter(lambda x: len(x) >3, [“I” , “want”, “to”, “learn”, “python”])
+A9 = filter(lambda x: len(x)>3,["I","want","to","learn","python"])
+#output = <filter object at 0x0000023573B9B1F0>
