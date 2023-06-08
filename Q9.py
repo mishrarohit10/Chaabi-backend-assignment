0884349b051932ba02ac185746e0dfb52e972814
mishrarohit10 <mishrarohit316x@gmail.com>
2023-06-08 13:10:42 +0530

    added input for Q9

--- Q9.py
+++ Q9.py
@@ -6,14 +6,20 @@ def func(from_date, to_date, diff):
     from_date = datetime.strptime(from_date, format)
     to_date = datetime.strptime(to_date, format)
 
-    delta =  to_date - from_date 
+    delta = abs(to_date - from_date)
     if delta.days < diff:
         return True
     else:
         return False
 
-str_d1 = '2021/10/20'
-str_d2 = '2021/10/20'
-diff = 10
+# str_d1 = '2021/10/20'
+# str_d2 = '2022/2/20'
+# diff = 10
+
+
+str_d1 = str(input('Enter from date in yyyy/mm/dd -> '))
+str_d2 = str(input('Enter to date in yyyy/mm/dd -> '))
+diff = int(input("Enter difference"))
+
 
 print(func(str_d1, str_d2 , diff))
